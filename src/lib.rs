f9eb402abf6a7c4f06510b57bd899e6d7eda766a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2022-01-20 16:39:25 +0900

    added recovery

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use std::{mem, ops::SubAssign};
+
 use num_bigint::{BigInt, RandBigInt};
 use num_traits::{One, Zero};
 use rand;
 
+#[derive(Clone, Debug)]
 pub struct SS {
     /// threshold
     pub t: usize,
@@ -43,6 +46,64 @@ impl SS {
             .rev()
             .fold(Zero::zero(), |sum, item| (&x_bigint * sum + item) % &self.p)
     }
+
+    pub fn recover(&self, shares: &[(usize, BigInt)]) -> BigInt {
+        assert!(shares.len() == self.t, "wrong shares");
+        let (xs, ys): (Vec<usize>, Vec<BigInt>) = shares.iter().cloned().unzip();
+        let result = self.lagrange_interpolation(Zero::zero(), xs, ys);
+        if result < Zero::zero() {
+            result + &self.p
+        } else {
+            result
+        }
+    }
+
+    fn lagrange_interpolation(&self, x: BigInt, xs: Vec<usize>, ys: Vec<BigInt>) -> BigInt {
+        let len = xs.len();
+        let xs_bigint: Vec<BigInt> = xs.iter().map(|x| BigInt::from(*x as i64)).collect();
+        (0..len).fold(Zero::zero(), |sum, item| {
+            let numerator = (0..len).fold(One::one(), |product: BigInt, i| {
+                if i == item {
+                    product
+                } else {
+                    product * (&x - &xs_bigint[i]) % &self.p
+                }
+            });
+            let denominator = (0..len).fold(One::one(), |product: BigInt, i| {
+                if i == item {
+                    product
+                } else {
+                    product * (&xs_bigint[item] - &xs_bigint[i]) % &self.p
+                }
+            });
+            (sum + numerator * self.mod_inv(denominator) * &ys[item]) % &self.p
+        })
+    }
+
+    fn mod_inv(&self, a: BigInt) -> BigInt {
+        let m = self.p.clone();
+        let num = if a < Zero::zero() { a + &self.p } else { a };
+        let (g, x, _) = SS::xgcd(num, m);
+        assert!(g.is_one());
+        (x + &self.p) % &self.p
+    }
+
+    fn xgcd(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
+        let (mut sj, mut sj_last) = (BigInt::zero(), BigInt::one());
+        let (mut tj, mut tj_last) = (BigInt::one(), BigInt::zero());
+        let (mut rj, mut rj_last) = (b, a);
+
+        while !rj.is_zero() {
+            let quotient = &rj_last / &rj;
+            rj_last.sub_assign(&quotient * &rj);
+            sj_last.sub_assign(&quotient * &sj);
+            tj_last.sub_assign(&quotient * &tj);
+            mem::swap(&mut rj, &mut rj_last);
+            mem::swap(&mut sj, &mut sj_last);
+            mem::swap(&mut tj, &mut tj_last);
+        }
+        (rj_last, sj_last, tj_last)
+    }
 }
 
 #[cfg(test)]
@@ -50,7 +111,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn recover_test() {
         let ss = SS {
             t: 3,
             n: 6,
@@ -73,6 +134,28 @@ mod tests {
                 (5, BigInt::from(1188)),
                 (6, BigInt::from(775)),
             ]
-        )
+        );
+        let r = ss.recover(&[
+            (1, BigInt::from(1494)),
+            (2, BigInt::from(329)),
+            (3, BigInt::from(965)),
+        ]);
+        assert_eq!(r, BigInt::from(1234))
+    }
+
+    #[test]
+    fn large_parime() {
+        let ss = SS {
+            t: 3,
+            n: 6,
+            p: BigInt::parse_bytes(
+                b"fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f",
+                16,
+            )
+            .unwrap(),
+        };
+        let secret = BigInt::parse_bytes(b"ffffffffffffffffffffffffffffffffffffff", 16).unwrap();
+        let shares = ss.split(secret.clone());
+        assert_eq!(secret, ss.recover(&shares[0..ss.t as usize]));
     }
 }
